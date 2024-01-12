462459e8fa0135d95539fae1607bbe69b52fd291
omegaalfa <omegaalfa.wr3@gmail.com>
2024-01-11 21:57:21 -0300

    Create AsyncTask.php

---
+++ src/AsyncTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace src\tasks;
+
+use Closure;
+
+class AsyncTask
+{
+
+	/**
+	 * @var callable
+	 */
+	protected $runnable;
+
+	/**
+	 * @param  callable  $runnable
+	 */
+	public function __construct(callable $runnable)
+	{
+		$this->runnable = $runnable;
+	}
+
+	/**
+	 * @return Closure
+	 */
+	public function run(): Closure
+	{
+		return function() {
+			call_user_func($this->runnable);
+		};
+	}
+}
