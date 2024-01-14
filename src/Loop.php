2f50dc6b2fbd377daa2d5f6d68a67c2d99466d59
omegaalfa <omegaalfa.wr3@gmail.com>
2024-01-13 21:30:13 -0300

    Create Loop.php

---
+++ src/Loop.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace OmegaAlfa\AsyncTask;
+
+class Loop
+{
+
+	/**
+	 * @var array
+	 */
+	protected array $callables;
+
+	/**
+	 * @var AsyncTaskManager
+	 */
+	protected AsyncTaskManager $AsyncTaskManager;
+
+	/**
+	 * @var AsyncTask
+	 */
+	protected AsyncTask $AsyncTask;
+
+
+	public function __construct()
+	{
+		$this->AsyncTaskManager = new AsyncTaskManager();
+	}
+
+
+	/**
+	 * @param  callable  $callable
+	 *
+	 * @return $this
+	 */
+	public function defer(callable $callable): static
+	{
+		$this->AsyncTask = new AsyncTask($callable);
+		$this->AsyncTaskManager->submit($this->AsyncTask);
+
+		return $this;
+	}
+
+	/**
+	 * @param  mixed     $name
+	 * @param  callable  $callable
+	 *
+	 * @return $this
+	 */
+	public function addTask(mixed $name, callable $callable): static
+	{
+		$this->AsyncTask = new AsyncTask($callable);
+		$this->AsyncTaskManager->submit($this->AsyncTask, $name);
+
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getResult(): mixed
+	{
+		return $this->AsyncTask->getResult();
+	}
+
+
+	/**
+	 * @param  mixed|null  $tasks
+	 *
+	 * @return void
+	 */
+	public function run(mixed $tasks = null): void
+	{
+		$this->AsyncTaskManager->run($tasks);
+	}
+}
